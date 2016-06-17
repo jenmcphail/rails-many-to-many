07f999fa49c8305302d13f9d656479012f6e96df
jenmcphail <mcphail.jen@gmail.com>
2016-06-16 18:18:53 -0700

    adds relationships in models

--- actors-movies/app/models/actor.rb
+++ actors-movies/app/models/actor.rb
@@ -1,2 +1,3 @@
 class Actor < ActiveRecord::Base
+	has_many :movies, through :role
 end
--- actors-movies/db/schema.rb
+++ actors-movies/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160617003851) do
+ActiveRecord::Schema.define(version: 20160617010223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 20160617003851) do
     t.string   "year"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "character"
+    t.integer  "actor_id"
+    t.integer  "movie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
